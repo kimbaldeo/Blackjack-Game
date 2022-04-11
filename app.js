56dc90766e36501b6eaa5001a7eabc7ff599070d
kbaldeoward <kbaldeoward@gmail.com>
2022-04-11 10:17:55 -0400

    adding to css and js

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+// important variables
+let playerTurn = true
+let dealerHand = []
+let dealerHandSum = 0
+let playerHand = []
+let playerHandSum = 0
+
+// Card Rendering Divs
+const playerDiv = document.querySelector(".player")
+const dealerDiv = document.querySelector(".dealer")
+
+// pulling html elements
+const hit = document.querySelector("#hit")
+const stand = document.querySelector("#stand")
+const newGame = document.querySelector("#new_game")
+let playerScore = document.querySelector("#player_score")
+let dealerScore = document.querySelector("#dealer_score")
+
+hit.addEventListener("click", function click() {
+    hitMe()
+})
+stand.addEventListener("click", function click() {
+    let playerTurn = false
+    dealerPlay()
+})
+newGame.addEventListener("click", function click() {
+    resetGame()
+    createDeck()
+    shuffle()
+    setup()
+})
+
+playerScore.innerHTML = playerHandSum
+dealerScore.innerHTML = dealerHandSum
+
 // creating card deck
 const suit = ["hearts", "spades", "clubs", "diamonds"]
 const value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
@@ -31,36 +66,6 @@ function shuffle() {
     return cardDeck
 }
 
-
-// important variables
-let playerTurn = true
-let dealerHand = []
-let dealerHandSum = 0
-let playerHand = []
-let playerHandSum = 0
-
-// Card Rendering Divs
-const playerDiv = document.querySelector(".player")
-const dealerDiv = document.querySelector(".dealer")
-
-// buttons
-const hit = document.querySelector("#hit")
-const stand = document.querySelector("#stand")
-const newGame = document.querySelector("#new_game")
-
-hit.addEventListener("click", function click() {
-    hitMe()
-})
-stand.addEventListener("click", function click() {
-    let playerTurn = false
-    dealerPlay()
-})
-newGame.addEventListener("click", function click() {
-    resetGame()
-    createDeck()
-    shuffle()
-    setup()
-})
 
 function resetGame() {
     playerHandSum = 0
@@ -170,6 +175,7 @@ function readValue(card) {
     }
     return weight
 }
+
 
 // after player stands, dealer completes their turn
 function dealerPlay() {
